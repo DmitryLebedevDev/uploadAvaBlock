16442063995cfac364dea3b874b88373701839b6
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2020-08-21 02:17:04 +0800

    Updates

---
+++ precache-manifest.e2e59aff56c32f57155570aed0ea6895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fe636fa92151aab3917e422455f279",
+    "url": "/uploadAvaBlock/index.html"
+  },
+  {
+    "revision": "323254fc07a24681e396",
+    "url": "/uploadAvaBlock/static/css/main.18e63f31.chunk.css"
+  },
+  {
+    "revision": "f7aad343fd3423372769",
+    "url": "/uploadAvaBlock/static/js/2.e2f748e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/uploadAvaBlock/static/js/2.e2f748e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323254fc07a24681e396",
+    "url": "/uploadAvaBlock/static/js/main.12db4bc6.chunk.js"
+  },
+  {
+    "revision": "e4ce3a4fdfa3ad93f79f",
+    "url": "/uploadAvaBlock/static/js/runtime-main.b5352d38.js"
+  }
+]);
